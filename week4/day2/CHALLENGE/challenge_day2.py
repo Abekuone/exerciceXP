6b0df0446e1c3b3a642dd56d68fb24815a7d45f5
Sorb-Kyénegnine Armel BEKUONE SOME <120484548+SBEKUONE@users.noreply.github.com>
2023-01-10 15:19:47 +0000

    My EXO_XP DAY2

---
+++ week4/day2/CHALLENGE/challenge_day2.py
@@ -0,0 +1,28 @@
+number=int(input("Enter a number : "))
+ln=int(input("Enter the lenth of the list : "))
+list_number=[]
+for i in range(1,ln+1):
+	list_number.append(i*number)
+print(list_number)
+print()
+
+#########################################################
+
+word=input("Enter a word : ")
+list_word=word.strip()
+new_list_word = []
+for words in list_word:
+  if words not in new_list_word:
+    new_list_word.append(words)
+word2=''.join(new_list_word)
+print(word," ==============> ",word2,"\n")
+
+
+
+
+
+
+
+
+
+
